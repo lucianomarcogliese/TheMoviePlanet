9a960f57dad49860e3c726cbfaa8c24a63e87e7e
JuaniBedoian <jbedoian@udesa.eduar>
2020-05-25 10:42:16 -0300

    validacion resenas (sin terminar)

--- controllers/resenaController.js
+++ controllers/resenaController.js
@@ -8,6 +8,19 @@ let resenaController = {
     
                      res.render("resena");
     
+    /* validar pelicula    */
+
+    function validarPelicula(pelicula){
+      let errores= [];
+      if (pelicula.title == "") {
+        errores.push("Por favor escribir el titulo completo!")
+      } else if (pelicula.title.lenght < 3) {
+        errores.push ("Escribir como minimo 3 caracteres!")
+      }
+      if (isNaN (pelicula.lenght)){
+        errores.push ("La duracion debe ser un numero!")
+      }
+    }
     },
 
     guardado: function(req, res) {
@@ -31,6 +44,21 @@ let resenaController = {
                                 id_usuarios : usuarios.id,    
                                 id_peliculas: req.query.idDePeli  
                            
+                   }
+
+
+                   /* 2da parte de validacion de pelicula */
+                   let errores = validarPelicula(pelicula)
+
+                   if (errores.length > 0) {
+                     //si hubieron errores => mostrar pagina con el formulario y los errores
+                     db.Genero.findAll()
+                     .then(generos) {
+                       res.render("". {           //no se que view poner ahi, 
+                         peliculas:pelicula,
+                         errores: errores
+                       })
+                     }
                    }
                                 
                                 
